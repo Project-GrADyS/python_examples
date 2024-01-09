505b35617aa605875d7896178458a063266987f9
Thiago Lamenza <thlamenza@hotmail.com>
2024-01-09 06:21:46 -0300

    All mobile nodes should start at the first waypoint in the mission (the ground station)

--- zigzag/main.py
+++ zigzag/main.py
@@ -1,11 +1,11 @@
 from gradysim.simulator.handler.communication import CommunicationHandler, CommunicationMedium
 from gradysim.simulator.handler.mobility import MobilityHandler
 from gradysim.simulator.handler.timer import TimerHandler
-from gradysim.simulator.handler.visualization import VisualizationHandler
-from gradysim.simulator.simulation import SimulationBuilder, SimulationConfiguration
 from gradysim.simulator.handler.visualization import VisualizationHandler, VisualizationConfiguration
-from protocol_sensor import ZigZagProtocolSensor
+from gradysim.simulator.simulation import SimulationBuilder, SimulationConfiguration
+
 from protocol_mobile import ZigZagProtocolMobile
+from protocol_sensor import ZigZagProtocolSensor
 
 
 def run_simulation(real_time: bool):
@@ -14,24 +14,27 @@ def run_simulation(real_time: bool):
     builder.add_handler(TimerHandler())
     builder.add_handler(MobilityHandler())
 
-    builder.add_handler(VisualizationHandler(VisualizationConfiguration(open_browser=True)))
+    builder.add_handler(VisualizationHandler(VisualizationConfiguration(open_browser=False,
+                                                                        x_range=(-1000, 1000),
+                                                                        y_range=(-1000, 1000),
+                                                                        z_range=(-1000, 1000))))
 
     builder.add_node(ZigZagProtocolSensor, (-600.0, 0.0, 0.0))
-    
+
     # Drone locations 
     builder.add_node(ZigZagProtocolMobile, (-600.0, 0.0, 0.0))
-    builder.add_node(ZigZagProtocolMobile, (-300.0, 0.0, 0.0))
-    
+    builder.add_node(ZigZagProtocolMobile, (-600.0, 0.0, 0.0))
+
     # Sensor locations 
     builder.add_node(ZigZagProtocolSensor, (-300.0, 0.0, 0.0))
     builder.add_node(ZigZagProtocolSensor, (0.0, 0.0, 0.0))
     builder.add_node(ZigZagProtocolSensor, (300.0, 0.0, 0.0))
     builder.add_node(ZigZagProtocolSensor, (600.0, 0.0, 0.0))
-    
+
     # Simulation
     simulation = builder.build()
     simulation.start_simulation()
 
 
 if __name__ == '__main__':
-    run_simulation(False)
+    run_simulation(True)
